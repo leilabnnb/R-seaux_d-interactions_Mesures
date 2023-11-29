2bbe73834d43457c2cbc089dd0b7df32cc5fb42a
Leila Bennabi <safa-leila.bennabi@etu.univ-lehavre.fr>
2023-11-29 10:37:16 +0100

    Q1 et début Q2

---
+++ tp1_mesures/src/main/java/org/example/Mesures.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import org.graphstream.algorithm.Toolkit;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.stream.file.FileSource;
+import org.graphstream.stream.file.FileSourceEdge;
+
+import java.io.IOException;
+
+public class Mesures {
+    public static void main(String[] args) {
+        Graph g = new DefaultGraph("g");
+        FileSource fs = new FileSourceEdge();
+
+        fs.addSink(g);
+
+        try {
+            fs.readAll("/home/etudiant/BS204037/RI/tp1_mesures/data/com-dblp.ungraph.txt");
+        } catch( IOException e) {
+            e.printStackTrace();
+        } finally {
+            fs.removeSink(g);
+        }
+
+    // Q2 Mesures de base
+
+        int order = g.getNodeCount();
+        int size = g.getEdgeCount();
+        double avgDegree = Toolkit.averageDegree(g);
+        double clusteringCoeffG = Toolkit.averageClusteringCoefficient(g);
+        System.out.println("Nombre de noeuds " + order + "\n"+
+                            "Nombre de liens " + size + "\n"+
+                            "Degré moyen " + avgDegree + "\n"+
+                            "Coefficient de clustering " + clusteringCoeffG + "\n"
+        );
+
+        // générer un graphe aléatoire
+        Graph gAlea = new DefaultGraph("gAlea");
+        Generators.randomNetwork(gAlea, order, avgDegree);
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
