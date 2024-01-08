799b13f0591ac070e2fd339199ec5c0ec5e1e7de
bs204037 <safa-leila.bennabi@etu.univ-lehavre.fr>
2024-01-08 23:34:55 +0100

    Propagation 2 processus d'infection

--- tp_propagation/src/main/java/org/example/Propagation.java
+++ tp_propagation/src/main/java/org/example/Propagation.java
@@ -8,20 +8,39 @@ import static org.graphstream.algorithm.Toolkit.averageDegree;
 public class Propagation {
 
 
-    public static double averageCarres(Graph g){
+    public static double averageCarres(Graph g) {
         double sommeCarres = 0.0;
         for (Node node : g) {
             int degree = node.getDegree();
-            sommeCarres+= degree * degree;
+            sommeCarres += degree * degree;
         }
 
         return sommeCarres / g.getNodeCount();
     }
-    public static double seuilEpidRéel(Graph g){
+
+    public static double seuilEpidRéel(Graph g) {
         return averageDegree(g) / averageCarres(g);
     }
 
+    public static double seuilEpidAlea(Graph g) {
+        return 1 / (averageDegree(g) + 1);
+    }
 
-
+    /**
+     * Fonction permettant de simuler le processus d'infection d'un noeud passé en paramètre
+     *
+     * @param n noeud à infecter sous certaines conditions
+     * @return le nombre d'infectés
+     */
+    public static void infecte(Node n) {
+        double probaDInfection = Math.random();
+        double taux_infection = 1.0 / 7.0;
+        if (probaDInfection < taux_infection && !n.hasAttribute("immunisé")) {
+            if (!n.hasAttribute("infecté")) {
+                // donc on est sur une personne qui n'a pas été immunisée et qui n'est pas infectée actuellement
+                n.setAttribute("infecté", true);
+            }
+        }
+    }
 
 }
