57113b0cf30c9b4aed4a3ce245e4f7e544cfb822
bs204037 <safa-leila.bennabi@etu.univ-lehavre.fr>
2024-01-09 00:37:17 +0100

    Propagation 2 scénario 1 contamination et immunisation

--- tp_propagation/src/main/java/org/example/Propagation.java
+++ tp_propagation/src/main/java/org/example/Propagation.java
@@ -30,17 +30,40 @@ public class Propagation {
     /**
      * Fonction permettant de simuler le processus d'infection d'un noeud passé en paramètre
      * @param voisin noeud à infecter sous certaines conditions
+     * @param infections nombre de noeuds inféctés
+     * @return nombre de noeuds inféctés
      */
-    public static void infecte(Node voisin) {
+    public static int infecte(Node voisin, int infections) {
         double probaDInfection = Math.random();
         double taux_infection = 1.0 / 7.0;
         if (probaDInfection < taux_infection && !voisin.hasAttribute("immunisé")) {
             if (!voisin.hasAttribute("infecté")) {
                 // donc on est sur une personne qui n'a pas été immunisée et qui n'est pas infectée actuellement
                 voisin.setAttribute("infecté", true);
+                infections++;
             }
         }
+        return infections;
     }
+
+    /**
+     * Fonction permettant de simuler le processus d'immunisation d'un noeud passé en paramètre
+     * @param voisin noeud à infecter sous certaines conditions
+     * @param infections nombre de noeuds inféctés
+     * @return nombre de noeuds inféctés
+     */
+    public static int immunise(Node voisin, int infections){
+        double probaDeGuérison = Math.random();
+        double taux_guérison = 2.0/30.0;
+        if(probaDeGuérison < taux_guérison){
+            if(voisin.hasAttribute("infecté")){
+                voisin.removeAttribute("infecté");
+                infections--;
+            }
+        }
+        return infections;
+    }
+
 
     public static void scénario1(Graph g){
         Node patient0 = g.getNode(0);
@@ -49,13 +72,19 @@ public class Propagation {
         for(int i = 0; i<90; i++){
             for(Node n : g){
                 if(n.hasAttribute("infecté")){ // patient0 sera le premier à vérifier la condition donc premier à contaminer tous ses collègues
-                    for(Edge e : n){
-                        infecte(e.getOpposite(n));
-                        infections++;
+                    for(Edge e : n){ // accéder à tous les voisins
+                        infections = infecte(e.getOpposite(n), infections);
                     }
                 }
+                infections = immunise(n, infections); // utilisation de l'anti virus si proba assez élevée
             }
         }
     }
+
+    public static void scénario2(Graph g) {
+
+    }
+
+
 
 }
